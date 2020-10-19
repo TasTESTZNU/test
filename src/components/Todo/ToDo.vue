de6e1f93ac7e75e0d79c92f13d4912891589d54e
Kishka <tas@mail.znu.edu.ua>
2020-10-19 21:01:46 +0300

    first commit

---
+++ src/components/Todo/ToDo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    New todo componet will be here!
+    <div>
+</template>
+
+<script>
+export default {
+  name: "ToDo"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/todo/TodoPage.vue
@@ -0,0 +1,15 @@
+<template>
+<div class = "todo-page">
+
+  <div>
+</template>
+
+<script>
+export default {
+name: "TodoPage"
+}
+</script>
+
+<style scoped>
+
+</style>
